47ae7482ed858dd6f1eb113fb34f8b9a1d04c5b3
Ak9312 <129245931+Ak9312@users.noreply.github.com>
2024-11-13 10:27:04 +0530

    Added result div

--- src/Component/PokemonCard/PokemonCard.jsx
+++ src/Component/PokemonCard/PokemonCard.jsx
@@ -5,20 +5,16 @@ export default function PokemonCard(props) {
 
   return (
     <>
- 
-   
-      <div className="  box-border h-72 w-72 border-4 rounded-xl		 ">
+      <div className="box-border h-72 w-72 border-4 rounded-xl">
         <p className="text-center	text-slate-500">#{id}</p>
         <div className="flex justify-center	my-6">
-        <div  className="  h-1/2 w-1/2 "> 
-          <img src={thumbnail} className="h-32 w-32 " />
+          <div className="  h-1/2 w-1/2 ">
+            <img src={thumbnail} className="h-32 w-32 " />
           </div>
         </div>
         <p className="text-center text-3xl	 font-bold capitalize">{name}</p>
-   <p className="text-center	">Captured</p>
+        <p className="text-center	">Captured</p>
       </div>
-     
-     
     </>
   );
 }
